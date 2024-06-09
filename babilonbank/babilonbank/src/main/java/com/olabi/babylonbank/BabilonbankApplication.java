6209bae99deae38e8ef42c338bcc54b731831b6f
hugomacielads <hugomacielcesar@gmail.com>
2024-06-09 16:36:28 -0300

    feat : Modelo Cliente

--- babilonbank/babilonbank/src/main/java/com/olabi/babylonbank/BabilonbankApplication.java
+++ babilonbank/babilonbank/src/main/java/com/olabi/babylonbank/BabilonbankApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BabylonbankApplication {
+public class BabilonbankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BabylonbankApplication.class, args);
+		SpringApplication.run(BabilonbankApplication.class, args);
 	}
 
 }
---
+++ babilonbank/babilonbank/src/main/java/com/olabi/babylonbank/model/entity/Cliente.java
@@ -0,0 +1,76 @@
+package com.olabi.babylonbank.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private double rendaMensal;
+
+    @Enumerated(EnumType.STRING)
+    private CategoriaCliente categoria;
+
+    @OneToOne(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private Conta conta;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getRendaMensal() {
+        return rendaMensal;
+    }
+
+    public void setRendaMensal(double rendaMensal) {
+        this.rendaMensal = rendaMensal;
+    }
+
+    public CategoriaCliente getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaCliente categoria) {
+        this.categoria = categoria;
+    }
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
+}
