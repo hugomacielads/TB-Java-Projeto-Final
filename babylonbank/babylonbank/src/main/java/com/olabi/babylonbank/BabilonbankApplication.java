21c071bccfa1cd0658321c8bf631becdc28df2bb
hugomacielads <hugomacielcesar@gmail.com>
2024-06-09 14:12:27 -0300

    Commit Inicial
    Sistema bancário Java. Springboot Application. BabylonBank

---
+++ babylonbank/babylonbank/src/main/java/com/olabi/babylonbank/BabilonbankApplication.java
@@ -0,0 +1,13 @@
+package com.olabi.babylonbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabylonbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabylonbankApplication.class, args);
+	}
+
+}
---
+++ babylonbank/babylonbank/src/test/java/com/olabi/babilonbank/BabilonbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olabi.babilonbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabilonbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
