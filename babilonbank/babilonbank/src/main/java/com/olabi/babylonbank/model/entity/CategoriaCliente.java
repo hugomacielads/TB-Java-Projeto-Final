a2cdab39471536f3372cd30ea80dd43bac24c3d7
hugomacielads <hugomacielcesar@gmail.com>
2024-06-09 22:49:16 -0300

    feat : Modelo Conta e Categoria Cliente

---
+++ babilonbank/babilonbank/src/main/java/com/olabi/babylonbank/model/entity/CategoriaCliente.java
@@ -0,0 +1,5 @@
+package com.olabi.babylonbank.model.entity;
+
+public enum CategoriaCliente {
+    BRONZE, PRATA, OURO;
+}
---
+++ babilonbank/babilonbank/src/main/java/com/olabi/babylonbank/model/entity/Conta.java
@@ -0,0 +1,70 @@
+package com.olabi.babylonbank.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double saldo;
+    private double chequeEspecial;
+    private double limiteCartaoCredito;
+
+    @OneToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @OneToMany(mappedBy = "conta", cascade = CascadeType.ALL)
+    private List<Transacao> transacoes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getChequeEspecial() {
+        return chequeEspecial;
+    }
+
+    public void setChequeEspecial(double chequeEspecial) {
+        this.chequeEspecial = chequeEspecial;
+    }
+
+    public double getLimiteCartaoCredito() {
+        return limiteCartaoCredito;
+    }
+
+    public void setLimiteCartaoCredito(double limiteCartaoCredito) {
+        this.limiteCartaoCredito = limiteCartaoCredito;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Transacao> getTransacoes() {
+        return transacoes;
+    }
+
+    public void setTransacoes(List<Transacao> transacoes) {
+        this.transacoes = transacoes;
+    }
+
+// getters e setters
+}
